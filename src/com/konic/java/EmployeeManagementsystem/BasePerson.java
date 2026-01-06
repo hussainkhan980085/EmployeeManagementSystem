cac3480e094cf83406d374d21cd95f7c9ce45dc9
hussainkhan980085 <hussainkhan980085@gmail.com>
2026-01-06 10:58:11 +0530

    Commit

--- src/com/konic/java/EmployeeManagementsystem/BasePerson.java
+++ src/com/konic/java/EmployeeManagementsystem/BasePerson.java
@@ -4,6 +4,11 @@ public class BasePerson {
 
 	protected int id;
 	protected String name;
+	
+	 protected BasePerson(int id, String name) {
+	        this.id = id;
+	        this.name = name;
+	    }
 
 	public int getId() {
 		return id;
